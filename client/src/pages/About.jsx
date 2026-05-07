a3420cf35e5e99bcffb018810cb6dcec9223ca49
MuhammadFaizan144 <fg7829098@gmail.com>
2026-05-07 16:49:33 +0500

    complete hero in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <main>
+      <section className='relative'>
+        <img src="/images/AboutUs/AboutHero.png" alt="" className='w-full h-[603px] object-cover'/>
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent z-10 "></div>
+        <div className="z-20 absolute flex justify-center flex-col text-center top-1/2 left-1/2 -translate-x-1/2 ">
+          <h1 className='lg:text-[60px] md:text-[40px] text-[40px] font-dm text-white font-bold'>About Vaxterm LLC</h1>
+          <p className='lg:text-[20px] md:text-[15px] text-[16px] font-dm text-white'>Delivering Reliable Technology, Strategic Procurement, and Government Compliant Solutions.</p>
+        </div>
+      </section>
+    </main>
   )
 }
 
